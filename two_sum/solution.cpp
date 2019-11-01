a8f1eea0fc048ec5600acc09b8155ba4572d3270
weiminxu <weiminxu1982@gmail.com>
2019-11-02 00:56:30 +1300

    two sum
    two sum

---
+++ two_sum/solution.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "solution.h"
+using namespace std;
+
+vector<int> solution::twoSum(vector<int>& nums, int target)
+{
+
+	return vector<int>();
+};
---
+++ two_sum/solution.h
@@ -0,0 +1,21 @@
+#ifndef _SOLUTION_H
+#define _SOLUTION_H
+
+#include <vector>
+using namespace std;
+
+class solution
+{
+public:
+
+	std::vector<int> twoSum(std::vector<int>& nums, int target);
+
+private:
+	std::vector<int>& number;
+	int sum;
+
+};
+
+#endif // !_SOLUTION_H
+
+
