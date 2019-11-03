d0542d52efdb76217ec044636c39ce12a36e663f
weiminxu <weiminxu1982@gmail.com>
2019-11-03 15:13:06 +1300

    for script to illustrate concurrent threads
    for script to illustrate concurrent threads

--- two_sum/main.cpp
+++ two_sum/main.cpp
@@ -9,6 +9,9 @@ Given nums = [2, 7, 11, 15], target = 9,
 
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
+
+https://leetcode.com/problems/two-sum/discuss/417369/C%2B%2B-Very-Simple-O(N)
+
 */
 
 #include "solution.h"
